44d788d4fc21497f6dd9b7be8976f6bba973d38d
KolManis <nikola.ilyin03@yandex.ru>
2025-12-19 20:20:44 +0500

    Add MongoDB database connection

---
+++ database/databaseConnection.go
@@ -0,0 +1,49 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBinstance() *mongo.Client {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	MongoDB := os.Getenv("MONGO_URL")
+
+	clientOptions := options.Client().ApplyURI(MongoDB)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal("MongoDB ping failed:", err)
+	}
+
+	fmt.Println("Connect to MongoDB")
+
+	return client
+}
+
+var Client *mongo.Client = DBinstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("cluster0").Collection(collectionName)
+	return collection
+}
