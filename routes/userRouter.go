ffdb0db2f0992fd0b22ccc1caf78417d17d91741
KolManis <nikola.ilyin03@yandex.ru>
2025-12-19 17:53:29 +0500

    Add user routes with auth middleware

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	controller "go-authentication/controllers"
+	"go-authentication/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.Use(middleware.Authenticate())
+	incomingRoutes.GET("users", controller.GetUsers())
+	incomingRoutes.GET("users/:user_id", controller.GetUser())
+}
