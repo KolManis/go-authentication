ed5458a4cd84274f0ed8295150e33306c636fa7c
KolManis <nikola.ilyin03@yandex.ru>
2025-12-20 21:51:28 +0500

    Чем ближе крах империи, тем страшнее коммиты

--- controllers/userControllerr.go
+++ controllers/userControllerr.go
@@ -12,20 +12,38 @@ import (
 	"go-authentication/database"
 	"go-authentication/models"
 
+	helper "github.com/KolManis/go-authentication/helpers"
 	"github.com/KolManis/go-authentication/models"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
 var vaildate = validator.New()
 
-func HashPassword()
+func HashPassword(password string) string {
+	bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		log.Panic(err)
+	}
+	return string(bytes)
+}
 
-func VerifyPassword()
+func VerifyPassword(userPassword string, providedPassword string) (passwordIsValid bool, msg string) {
+	err := bcrypt.CompareHashAndPassword([]byte(providedPassword), []byte(userPassword))
+	check := true
+	msg := ""
+
+	if err != nil {
+		msg = fmt.Sprint("email of password is incorrect")
+		check = false
+	}
+
+}
 
 func Singup() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -50,6 +68,9 @@ func Singup() gin.HandlerFunc {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while checking for the email"})
 			//return
 		}
+
+		password := HashPassword(*user.Password)
+		user.Password = &password
 
 		phoneCount, err := userCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
 		if err != nil {
@@ -102,7 +123,28 @@ func Login() gin.HandlerFunc {
 		}
 
 		passwordIsValid, msg := VerifyPassword(*user.Password, *foundUser.Password)
+		if passwordIsValid != true {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
 
+		//соль
+
+		if foundUser.Email == nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "user not found"})
+		}
+
+		token, refreshToken, _ := helper.GenerateAllTokens(*foundUser.Email, *foundUser.First_name, *foundUser.Last_name, *foundUser.User_type, foundUser.User_id)
+		helper.UpdateAllTokens(token, refreshToken, foundUser.User_id)
+
+		err = userCollection.FindOne(ctx, bson.M{"user_id": foundUser.User_id}).Decode(&foundUser)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, foundUser)
 	}
 }
 
--- helpers/tokenHelper.go
+++ helpers/tokenHelper.go
@@ -1,13 +1,17 @@
 package helper
 
 import (
+	"context"
 	"log"
 	"os"
 	"time"
 
 	"github.com/KolManis/go-authentication/database"
 	jwt "github.com/dgrijalva/jwt-go"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type SignedDetails struct {
@@ -55,4 +59,45 @@ func GenerateAllTokens(
 	}
 
 	return token, refreshToken, err
+}
+
+func UpdateAllTokens(signedtoken string, signedRefreshToken string, userId string) error   {
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+
+	var updateObj primitive.D
+
+	updateObj = append(updateObj, bson.E{"token", signedtoken})
+	updateObj = append(updateObj, bson.E{"refresh_token", signedRefreshToken})
+
+	Update_at, _ := time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	updateObj = append(updateObj, bson.E{"update_at", Update_at})
+
+	// primitive.D{
+    // {"token", "eyJhbGciOiJ..."},
+    // {"refresh_token", "eyJhbGciOiJ..."},
+    // {"update_at", "2024-01-15T10:30:00Z"},
+
+	upsert := true
+	filter := bson.M{"user_id": userId}
+	opt := options.UpdateOptions{
+		Upsert: &upsert,
+	}
+
+	
+	_, err := userCollection.UpdateOne(
+		ctx, 
+		filter,
+		bson.D{
+			{"$set", updateObj}
+		},
+		&opt
+	)
+
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+
+	return
 }
